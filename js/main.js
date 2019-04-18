8ab05cab87ff6f3072949fbf04ec49b9d15c6928
Joel Venable <joelmusicman@gmail.com>
2019-04-18 10:04:39 -0500

    Finished resize on scroll functionality

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-const audrey = document.getElementById("audrey")
+console.log('hello from main.js')
 
-  /*
-      Add an event listener to the `document` object to listen
-      for the "scroll" event.
-  */
-  .addEventListener("", function () {
-    /*
-        Adjust the width of audrey to be 1/3 the value of
-        `window.scrollY`. No lower than 50px, though.
-    */
+const audrey = document.getElementById("audrey");
 
 
+document.addEventListener("scroll", function(event) {
 
-    /*
-        Adjust the height of audrey to be 1/4 the value of
-        `window.scrollY`. No lower than 100px, though.
-    */
-  })
+  let windowX = window.scrollX / 4,
+    windowY = window.scrollY / 3,
+    x = Math.max(windowX, 50),
+    y = Math.max(windowY, 100);
+  audrey.style.width = `${x}px`;
+  audrey.style.height = `${y}px`;
+
+
+});
